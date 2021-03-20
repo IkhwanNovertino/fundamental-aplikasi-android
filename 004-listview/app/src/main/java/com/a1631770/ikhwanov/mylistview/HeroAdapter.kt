9e58268c1f1d47300bcca8cce7a32743ecb97e51
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-03-20 22:02:22 +0800

    menyelesaikan project ListView

---
+++ 004-listview/app/src/main/java/com/a1631770/ikhwanov/mylistview/HeroAdapter.kt
@@ -0,0 +1,45 @@
+package com.a1631770.ikhwanov.mylistview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
+  internal var heroes = arrayListOf<Hero>()
+  override fun getCount(): Int = heroes.size
+
+  override fun getItem(position: Int): Any = heroes[position]
+
+  override fun getItemId(position: Int): Long = position.toLong()
+
+  override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    var itemView = convertView
+    if (itemView == null) {
+      itemView = LayoutInflater.from(context).inflate(R.layout.item_hero, parent, false)
+    }
+
+    val viewHolder = ViewHolder(itemView as View)
+
+    val hero = getItem(position) as Hero
+    viewHolder.bind(hero)
+    return itemView
+
+  }
+
+  private inner class ViewHolder internal constructor(view: View) {
+    private val txtName = view.findViewById<TextView>(R.id.txt_name)
+    private val txtDesc = view.findViewById<TextView>(R.id.txt_desc)
+    private val imgPhoto = view.findViewById<ImageView>(R.id.img_photo)
+
+    internal fun bind(hero: Hero) {
+      txtName.text = hero.name
+      txtDesc.text = hero.desc
+      imgPhoto.setImageResource(hero.photo)
+    }
+  }
+}
--- 004-listview/app/src/main/java/com/a1631770/ikhwanov/mylistview/MainActivity.kt
+++ 004-listview/app/src/main/java/com/a1631770/ikhwanov/mylistview/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.a1631770.ikhwanov.mylistview
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var adapter: HeroAdapter
+  private lateinit var dataName: Array<String>
+  private lateinit var dataDesc: Array<String>
+  private lateinit var dataPhoto: TypedArray
+  private var heroes = arrayListOf<Hero>()
+
+//  private val dataName = arrayOf(
+//    "Cut Nyak Dien",
+//    "Ki Hajar Dewantara",
+//    "Moh Yamin",
+//    "Pattimura",
+//    "R. A. Kartini",
+//    "Sukarno")
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    /**
+     * menggunakan dataName diatas
+    val listView: ListView = findViewById(R.id.lv_list)
+    val adapter = ArrayAdapter<String>(
+      this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+
+    listView.adapter = adapter
+    **/
+
+    //Menggunakan Hero class
+    val listView = findViewById<ListView>(R.id.lv_list)
+    adapter = HeroAdapter(this)
+    listView.adapter = adapter
+
+    //untuk inisiasi array
+    prepare()
+    //membuat method addItem() untuk memasukkan data tersebut
+    addItem()
+
+    listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+      Toast.makeText(this@MainActivity, heroes[position].name, Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  private fun prepare() {
+    dataName = resources.getStringArray(R.array.data_name)
+    dataDesc = resources.getStringArray(R.array.data_desc)
+    dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+  }
+
+  private fun addItem() {
+    for (position in dataName.indices) {
+      val hero = Hero(
+          dataPhoto.getResourceId(position, -1),
+          dataName[position],
+          dataDesc[position]
+      )
+
+      heroes.add(hero)
+    }
+    adapter.heroes = heroes
   }
 }
