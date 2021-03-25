9358f8e3aaec27f2569a5b379324f441b7221d8f
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-03-25 13:20:54 +0800

    melakukan error IndexOutOfBoundsException

--- 005-debugging/app/src/main/java/com/a1631770/ikhwanov/mytestingapp/MainActivity.kt
+++ 005-debugging/app/src/main/java/com/a1631770/ikhwanov/mytestingapp/MainActivity.kt
@@ -2,13 +2,17 @@ package com.a1631770.ikhwanov.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
   private lateinit var btnSetValue: Button
   private lateinit var tvText: TextView
+
+  private var names = ArrayList<String>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -18,11 +22,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     btnSetValue = findViewById(R.id.btn_set_value)
 
     btnSetValue.setOnClickListener(this)
+
+    names.add("Muhammad Ikhwan")
+    names.add("Ikhwan")
+    names.add("Kelvin")
   }
 
   override fun onClick(v: View) {
     if (v.id == R.id.btn_set_value) {
-      tvText.text = "19"
+      Log.d("MainActivity", names.toString())
+      val name = StringBuilder()
+      for (i in 0..3) {
+        name.append(names[i]).append("\n")
+      }
+      tvText.text = name.toString()
+//      tvText.text = "19"
     }
   }
 }
