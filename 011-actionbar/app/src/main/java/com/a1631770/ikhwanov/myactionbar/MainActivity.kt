c6afba3b158543414cfd9ad69169ab110c2d4735
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-04-06 16:26:31 +0800

    finish option-menu

--- 011-actionbar/app/src/main/java/com/a1631770/ikhwanov/myactionbar/MainActivity.kt
+++ 011-actionbar/app/src/main/java/com/a1631770/ikhwanov/myactionbar/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.a1631770.ikhwanov.myactionbar
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.a1631770.ikhwanov.myactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding : ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    val inflater = menuInflater
+    inflater.inflate(R.menu.option_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when(item.itemId) {
+      R.id.menu1 -> {
+        supportFragmentManager.beginTransaction()
+          .replace(R.id.fragment_container, MenuFragment())
+          .addToBackStack(null)
+          .commit()
+        return true
+      }
+      R.id.menu2 -> {
+        val intent = Intent(this@MainActivity, MenuActivity::class.java)
+        startActivity(intent)
+        return true
+      }
+      else -> return true
+    }
   }
 }
