dd520a5b00b85fee5ae24812da2c69cd384699f8
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-04-12 06:38:21 +0800

    menyelsaikan proses asynchronous menggunakan executor dan handler, kemudian menggunakan coroutines

--- 016-background-thread/app/src/main/java/com/a1631770/ikhwanov/mybackgroundthread/MainActivity.kt
+++ 016-background-thread/app/src/main/java/com/a1631770/ikhwanov/mybackgroundthread/MainActivity.kt
@@ -2,7 +2,15 @@ package com.a1631770.ikhwanov.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.a1631770.ikhwanov.mybackgroundthread.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +21,50 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    val executor = Executors.newSingleThreadExecutor()
+    val handler = Handler(Looper.getMainLooper())
+
     binding.btnStart.setOnClickListener {
+      //proses asynchronous menggunkan kotlin-coroutines
+      lifecycleScope.launch(Dispatchers.Default) {
+        //simulate process in background thread
+        for (i in 0..10) {
+          delay(500)
+          val percentage = i * 10
+          withContext(Dispatchers.Main) {
+            //update ui in main thread
+            if (percentage == 100) {
+              binding.tvStatus.setText(R.string.task_completed)
+            } else {
+              binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+            }
+          }
+        }
+      }
+
+      /**
+       * proses asychronous menggunkan executor dan handler
+      executor.execute {
+        try {
+          //simulate process in background thread
+          for (i in 0..10) {
+            Thread.sleep(500)
+            val percentage = i * 10
+            handler.post {
+              //update ui in main thread
+              if (percentage == 100) {
+                binding.tvStatus.setText(R.string.task_completed)
+              } else {
+                binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+              }
+            }
+          }
+        } catch (e : InterruptedException) {
+          e.printStackTrace()
+        }
+      }
+
+       * proses Synchronous
       try {
           //simulate process compressing
         for ( i in 0..10) {
@@ -28,6 +79,7 @@ class MainActivity : AppCompatActivity() {
       } catch (e : InterruptedException) {
         e.printStackTrace()
       }
+      */
     }
   }
 }
