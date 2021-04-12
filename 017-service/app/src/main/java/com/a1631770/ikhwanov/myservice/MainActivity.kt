7cd90d36f1dcc70d778a017472dc2df36d2d6861
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-04-13 07:58:32 +0800

    menyelesaikan modul BoundService

--- 017-service/app/src/main/java/com/a1631770/ikhwanov/myservice/MainActivity.kt
+++ 017-service/app/src/main/java/com/a1631770/ikhwanov/myservice/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.a1631770.ikhwanov.myservice
 
+import android.app.PendingIntent.getService
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.a1631770.ikhwanov.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding : ActivityMainBinding
+
+  //BoundService
+  private var mServiceBound = false
+  private lateinit var mBoundService: MyBoundService
+
+  private val mServiceConnection = object : ServiceConnection {
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+      val myBinder = service as MyBoundService.MyBinder
+      mBoundService = myBinder.getService
+      mServiceBound = true
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {
+      mServiceBound = false
+    }
+
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -25,9 +46,20 @@ class MainActivity : AppCompatActivity() {
       MyJobIntentService.enqueueWork(this, mStartJobIntentService)
     }
 
-    binding.btnStartBoundService.setOnClickListener {  }
+    binding.btnStartBoundService.setOnClickListener {
+      val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+      bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
+    }
 
-    binding.btnStopBoundService.setOnClickListener {  }
+    binding.btnStopBoundService.setOnClickListener {
+      unbindService(mServiceConnection)
+    }
+  }
 
+  override fun onDestroy() {
+    super.onDestroy()
+    if (mServiceBound) {
+      unbindService(mServiceConnection)
+    }
   }
 }
