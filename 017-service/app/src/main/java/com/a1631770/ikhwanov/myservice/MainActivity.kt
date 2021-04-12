b84e635b7ad6157b23c227164167b1429f0ef998
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-04-13 00:09:28 +0800

    menyelesaikan modul startService

--- 017-service/app/src/main/java/com/a1631770/ikhwanov/myservice/MainActivity.kt
+++ 017-service/app/src/main/java/com/a1631770/ikhwanov/myservice/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.a1631770.ikhwanov.myservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.a1631770.ikhwanov.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding : ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.btnStartService.setOnClickListener {
+      val mStartServiceIntent = Intent(this@MainActivity, MyService::class.java)
+      startService(mStartServiceIntent)
+    }
+
+    binding.btnStartJobIntentService.setOnClickListener {  }
+
+    binding.btnStartBoundService.setOnClickListener {  }
+
+    binding.btnStopBoundService.setOnClickListener {  }
+
   }
 }
