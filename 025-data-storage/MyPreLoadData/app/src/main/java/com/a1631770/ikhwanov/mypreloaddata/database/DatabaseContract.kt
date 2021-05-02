5c028b11dde721105f80648cfa0ca00a7cfc69e2
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-05-02 12:07:56 +0800

    finish SQLite Optimization module

---
+++ 025-data-storage/MyPreLoadData/app/src/main/java/com/a1631770/ikhwanov/mypreloaddata/database/DatabaseContract.kt
@@ -0,0 +1,15 @@
+package com.a1631770.ikhwanov.mypreloaddata.database
+
+import android.provider.BaseColumns
+
+internal object DatabaseContract {
+
+    var TABLE_NAME = "table_mahasiswa"
+
+    internal class MahasiswaColumns : BaseColumns {
+        companion object {
+            const val NAMA = "nama"
+            const val NIM = "nim"
+        }
+    }
+}
